2fb2a80c4f25ebb276b275aa9569834f4b8e5792
masp9 <mart2c04@stud.kea.dk>
2022-10-12 23:31:05 +0200

    refactor

--- js/deck.js
+++ js/deck.js
@@ -11,28 +11,28 @@ const renderDecks = data => {
     console.log(data);
     for (var i = 0; i < data.length; i++) {
          
-         let deckElement = document.createElement('div');
-         deckElement.classList.add('deck');
-         deckElement.addEventListener("click", e => {
+         let deckEle = document.createElement('div');
+         deckEle.classList.add('deck');
+         deckEle.addEventListener("click", e => {
             // Hvad sker der her?? hvorfor er den ene et object, men den anden undefined??
             console.log(data);
             console.log(data[i])
            
          });
     
-        let deckName = document.createElement('p');
-        deckName.classList.add('text')
-        deckName.textContent = data[i].name;
-        deckElement.appendChild(deckName);
+        let deckNameEle = document.createElement('p');
+        deckNameEle.classList.add('text')
+        deckNameEle.textContent = data[i].name;
+        deckEle.appendChild(deckNameEle);
         
-        let deckCategory = document.createElement('p');
-        deckCategory.classList.add('text')
-        deckCategory.textContent = data[i].category;
-        deckElement.appendChild(deckCategory);
+        let deckCategoryEle = document.createElement('p');
+        deckCategoryEle.classList.add('text')
+        deckCategoryEle.textContent = data[i].category;
+        deckEle.appendChild(deckCategoryEle);
         
-        deckContainer.appendChild(deckElement); 
+        deckContainer.appendChild(deckEle); 
         
-        console.log(deckElement);
+        console.log(deckEle);
         }
     
     
