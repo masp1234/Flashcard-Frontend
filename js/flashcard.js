d07dd70ecf1aacf2a9ff6ad3a43aaafd1e735e50
masp9 <mart2c04@stud.kea.dk>
2022-10-22 00:21:03 +0200

    add cardflip feature

--- js/flashcard.js
+++ js/flashcard.js
@@ -2,29 +2,49 @@ var getFlashcards = deck => {
     itemContainer.replaceChildren();
 
     deck.flashcards.forEach(flashcard => {
-        flashcardEle = document.createElement('div');
+        const flashcardEle = document.createElement('div');
         flashcardEle.classList.add('flashcard');
+        flashcardEle.classList.add('not-flipped');
         
-
-
-        flashcardQuestionTextEle = document.createElement('p');
+        const flashcardQuestionTextEle = document.createElement('p');
         flashcardQuestionTextEle.classList.add('text');
-        flashcardAnswerTextEle = document.createElement('p');
+
+        const flashcardAnswerTextEle = document.createElement('p');
         flashcardAnswerTextEle.classList.add('text');
-        flashcardPointsEle = document.createElement('p');
+        
+        const flashcardPointsEle = document.createElement('p');
         flashcardPointsEle.classList.add('text');
+
+        const activateMultipleChoiceButton = document.createElement('button');
+        activateMultipleChoiceButton.addEventListener("click", activateMultipleChoice);
 
         flashcardQuestionTextEle.textContent = flashcard.questionText;
         flashcardAnswerTextEle.textContent = flashcard.answerText;
         flashcardPointsEle.textContent = flashcard.points;
 
         flashcardEle.appendChild(flashcardQuestionTextEle);
-        flashcardEle.appendChild(flashcardAnswerTextEle);
         flashcardEle.appendChild(flashcardPointsEle);
         
         
         itemContainer.appendChild(flashcardEle);
-        
+
+        flashcardEle.addEventListener("click", () => {
+            if (flashcardEle.classList.contains('not-flipped')) {
+                flashcardEle.replaceChild(flashcardAnswerTextEle, flashcardQuestionTextEle);
+                flashcardEle.classList.replace('not-flipped', 'flipped');
+                
+            }
+            else {
+                flashcardEle.replaceChild(flashcardQuestionTextEle, flashcardAnswerTextEle);
+                flashcardEle.classList.replace('flipped', 'not-flipped');
+                
+            }
+
+        })
     } ) 
+    
+}
+
+const activateMultipleChoice = () => {
     
 }
